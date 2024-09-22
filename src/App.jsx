0e8b59cd914c37ef18f79edb1b910758da100b9f
EDWINI69 <darkbroks@gmail.com>
2024-09-22 19:32:51 -0400

    manin

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import Titulo from "./titulo";
+
+function App() {
+  return <Titulo></Titulo>;
+}
+
+export default App;
---
+++ src/titulo.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+
+function Titulo() {
+  const [count, setCount] = useState(0);
+
+  function Click() {
+    setCount(count + 1);
+  }
+
+  return (
+    <div>
+      <button type="button" class="btn btn-primary" onClick={Click}>
+        Primary
+      </button>
+
+      <input type="text" onChange={Click} />
+
+      <p>Ha presionado el boton {count} veces</p>
+    </div>
+  );
+}
+
+export default Titulo;
